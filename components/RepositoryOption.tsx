13ed4bfccfed38b8ace7612c19a50d03cb047c92
Cali Castle <hi@cali.so>
2023-03-23 16:52:57 +0800

    fix: eslint

--- components/RepositoryOption.tsx
+++ components/RepositoryOption.tsx
@@ -2,7 +2,11 @@ import React from 'react'
 import { Combobox } from '@headlessui/react'
 import Image from 'next/image'
 import { classNames } from '../lib/utils'
-import { CpuChipIcon, StarIcon, ViewfinderCircleIcon } from '@heroicons/react/24/outline'
+import {
+  CpuChipIcon,
+  StarIcon,
+  ViewfinderCircleIcon,
+} from '@heroicons/react/24/outline'
 
 export function RepositoryOption() {
   return (
@@ -61,7 +65,16 @@ export function RepositoryOption() {
               </span>
               {/* Forks */}
               <span className="flex items-center space-x-0.5">
-                <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 1024 1024" className="w-4 h-4" xmlns="http://www.w3.org/2000/svg"><path d="M752 100c-61.8 0-112 50.2-112 112 0 47.7 29.9 88.5 72 104.6v27.6L512 601.4 312 344.2v-27.6c42.1-16.1 72-56.9 72-104.6 0-61.8-50.2-112-112-112s-112 50.2-112 112c0 50.6 33.8 93.5 80 107.3v34.4c0 9.7 3.3 19.3 9.3 27L476 672.3v33.6c-44.2 15-76 56.9-76 106.1 0 61.8 50.2 112 112 112s112-50.2 112-112c0-49.2-31.8-91-76-106.1v-33.6l226.7-291.6c6-7.7 9.3-17.3 9.3-27v-34.4c46.2-13.8 80-56.7 80-107.3 0-61.8-50.2-112-112-112zM224 212a48.01 48.01 0 0 1 96 0 48.01 48.01 0 0 1-96 0zm336 600a48.01 48.01 0 0 1-96 0 48.01 48.01 0 0 1 96 0zm192-552a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path></svg>
+                <svg
+                  stroke="currentColor"
+                  fill="currentColor"
+                  strokeWidth="0"
+                  viewBox="0 0 1024 1024"
+                  className="w-4 h-4"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M752 100c-61.8 0-112 50.2-112 112 0 47.7 29.9 88.5 72 104.6v27.6L512 601.4 312 344.2v-27.6c42.1-16.1 72-56.9 72-104.6 0-61.8-50.2-112-112-112s-112 50.2-112 112c0 50.6 33.8 93.5 80 107.3v34.4c0 9.7 3.3 19.3 9.3 27L476 672.3v33.6c-44.2 15-76 56.9-76 106.1 0 61.8 50.2 112 112 112s112-50.2 112-112c0-49.2-31.8-91-76-106.1v-33.6l226.7-291.6c6-7.7 9.3-17.3 9.3-27v-34.4c46.2-13.8 80-56.7 80-107.3 0-61.8-50.2-112-112-112zM224 212a48.01 48.01 0 0 1 96 0 48.01 48.01 0 0 1-96 0zm336 600a48.01 48.01 0 0 1-96 0 48.01 48.01 0 0 1 96 0zm192-552a48.01 48.01 0 0 1 0-96 48.01 48.01 0 0 1 0 96z"></path>
+                </svg>
                 <span>211 forks</span>
               </span>
             </div>
